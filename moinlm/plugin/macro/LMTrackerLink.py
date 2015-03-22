bc16a0d8e4a054dea0c49c5b668354936af0f5b0
Noah Hoffman <nhoffman@positron.local>
2015-03-21 21:35:39 -0700

    update url in tracker link

--- moinlm/plugin/macro/LMTrackerLink.py
+++ moinlm/plugin/macro/LMTrackerLink.py
@@ -7,19 +7,9 @@
 """
 
 import sys
-import re
-import time
-import datetime
 import pprint
 
-from MoinMoin import search, wikiutil
-from MoinMoin.logfile import editlog
-from MoinMoin.Page import Page
-from MoinMoin.user import User
-from MoinMoin.util.dataset import TupleDataset, Column
-from MoinMoin.widget.browser import DataBrowserWidget
-
-__version__ = "$Rev: 3322 $"
+from MoinMoin import wikiutil
 
 Dependencies = ["time"]
 
@@ -29,34 +19,32 @@ def execute(self, argstr=None):
     argstr = argstr or ''
 
     request = self.request
-    _ = self._
 
     def format_err(msg):
         style = """color:red; border-style:dotted; padding:0.5em;
             background-color:#FFCCCC; width:75%; display:block"""
 
         return request.formatter.text(
-            ('Error in <<LMTrackerLink(%s)>> '%argstr) + msg,
-            style=style)
+            ('Error in <<LMTrackerLink(%s)>> ' % argstr) + msg, style=style)
 
     try:
         defaults = (
-            ('issue',None),
-            ('tracker','it'),
+            ('issue', None),
+            ('tracker', 'it'),
         )
         keys = [x[0] for x in defaults]
 
         args = dict(defaults)
         positional, kwargs, trailing = wikiutil.parse_quoted_separated(argstr or u'')
 
-        args.update(dict((k,v) for k,v in zip(keys,positional) if v))
+        args.update(dict((k, v) for k, v in zip(keys, positional) if v))
         args.update(kwargs)
 
         if not args['issue']:
             return format_err('Please provide a tracker issue number')
 
         url = """
-        <a href="https://web.labmed.washington.edu/tracker/%(tracker)s/roundup.cgi/%(tracker)s/issue%(issue)s"
+        <a href="https://tracker.labmed.uw.edu/%(tracker)s/issue%(issue)s"
         target="_blank">
         %(tracker)s:Issue%(issue)s
         </a>
@@ -66,8 +54,3 @@ def execute(self, argstr=None):
 
     except:
         return '<pre>%s</pre>' % pprint.pformat(sys.exc_info())
-
-
-
-
-
